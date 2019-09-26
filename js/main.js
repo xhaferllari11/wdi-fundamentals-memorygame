5f871286a8b267843015f4acf46983f201cc2446
ALBAN MAC <albanmac@ALBANs-MacBook-Pro.local>
2019-09-25 23:23:11 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("up and running");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardOne);
+
