8f3dcb075235bc770d479649d0559293bc028eeb
ALBAN MAC <albanmac@ALBANs-MacBook-Pro.local>
2019-09-27 22:57:52 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 console.log("up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('user flipped ' + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('user flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
 
